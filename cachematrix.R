4ce08135891b12c45082dc17a277ab92d13d0224
Polycad <56765480+Polycad@users.noreply.github.com>
2019-10-19 19:36:51 +0300

    PK: Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,36 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Matrix inversion is a costly operation.
+## There may be some benefit to caching the inverse of a matrix rather than compute it repeatedly.
+## This code make the pairs of functions that cache the inverse of a matrix.
 
-## Write a short comment describing this function
+## The function "makeCacheMatrix" is used for creating "matrix" object that can cache its inverse
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    p <- NULL
+  set <- function(y) {
+          x <<- y
+          p <<- NULL
+  }
+  get <- function() x
+  setinverse <- function(inverse) p <<- inverse
+  getinverse <- function() p
+  list(set = set,
+       get = get,
+       setinverse = setinverse,
+       getinverse = getinverse)
 }
 
-
-## Write a short comment describing this function
+## The function "cacheSolve" computes the inverse of the returned "matrix". 
+## Previosly calculated inverse of the "matrix" should be retrieve from the cache.
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  p <- x$getinverse()
+  if (!is.null(p)) {
+          message("getting cached data")
+          return(p)
+  }
+  data <- x$get()
+  p <- solve(data, ...)
+  x$setinverse(p)
+  p
 }
